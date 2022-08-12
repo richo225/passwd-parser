6829db069e7de92c16aa3fc0fdec7394f64b8588
Richard Bates <rich.bates@protonmail.com>
2022-08-12 13:38:40 +0800

    Move parser functions to separate package

--- src/main.go
+++ src/main.go
@@ -3,32 +3,21 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
-	"flag"
-	"fmt"
 	"io"
 	"os"
+	"passwd-parser/parser"
 	"strconv"
-	"strings"
-
-	"golang.org/x/exp/slices"
 )
 
-type User struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Home  string `json:"home"`
-	Shell string `json:"shell"`
-}
-
 func main() {
-	path, format := parseFlags()
-	users := collectUsers()
+	path, format := parser.ParseFlags()
+	users := parser.CollectUsers()
 
 	var output io.Writer
 	if path != "" {
 		// write user info to path in specified format
 		f, err := os.Create(path)
-		handleError(err)
+		parser.HandleError(err)
 		defer f.Close()
 		output = f
 	} else {
@@ -41,71 +30,14 @@ func main() {
 		writer := csv.NewWriter(output)
 		for _, user := range users {
 			err := writer.Write([]string{strconv.Itoa(user.Id), user.Name, user.Home, user.Shell})
-			handleError(err)
+			parser.HandleError(err)
 		}
 		writer.Flush()
 	}
 
 	if format == "json" {
 		data, err := json.MarshalIndent(users, "", "  ")
-		handleError(err)
+		parser.HandleError(err)
 		output.Write(data)
-	}
-}
-
-func parseFlags() (path, format string) {
-	flag.StringVar(&path, "path", "", "path to export file")
-	flag.StringVar(&format, "format", "json", "output format for the user information eg, csv, json")
-
-	flag.Parse()
-
-	// Check validity of format flag
-	format = strings.ToLower(format)
-	if !slices.Contains([]string{"csv", "json"}, format) {
-		fmt.Println("Error: invalid format. Use 'json' or 'csv' instead.")
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	return
-}
-
-func collectUsers() (users []User) {
-	f, err := os.Open("passwd")
-	handleError(err)
-	defer f.Close()
-
-	reader := csv.NewReader(f)
-	reader.Comma = ':'
-
-	lines, err := reader.ReadAll()
-	handleError(err)
-
-	for _, line := range lines {
-		id, err := strconv.ParseInt(line[2], 10, 64)
-		handleError(err)
-
-		// Don't want system users here
-		if id < 100 {
-			continue
-		}
-
-		user := User{
-			Id:    int(id),
-			Name:  line[0],
-			Home:  line[5],
-			Shell: line[6],
-		}
-
-		users = append(users, user)
-	}
-
-	return
-}
-
-func handleError(err error) {
-	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
 	}
 }
---
+++ src/parser/parser.go
@@ -0,0 +1,108 @@
+package parser
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"golang.org/x/exp/slices"
+)
+
+type User struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Home  string `json:"home"`
+	Shell string `json:"shell"`
+}
+
+func ParseFlags() (path, format string) {
+	flag.StringVar(&path, "path", "", "path to export file")
+	flag.StringVar(&format, "format", "json", "output format for the user information eg, csv, json")
+
+	flag.Parse()
+
+	// Check validity of format flag
+	format = strings.ToLower(format)
+	if !slices.Contains([]string{"csv", "json"}, format) {
+		fmt.Println("Error: invalid format. Use 'json' or 'csv' instead.")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	return
+}
+
+func CollectUsers() (users []User) {
+	f, err := os.Open("passwd")
+	HandleError(err)
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	reader.Comma = ':'
+
+	lines, err := reader.ReadAll()
+	HandleError(err)
+
+	for _, line := range lines {
+		id, err := strconv.ParseInt(line[2], 10, 64)
+		HandleError(err)
+
+		// Don't want system users here
+		if id < 100 {
+			continue
+		}
+
+		user := User{
+			Id:    int(id),
+			Name:  line[0],
+			Home:  line[5],
+			Shell: line[6],
+		}
+
+		users = append(users, user)
+	}
+
+	return
+}
+
+func WriteOutput(path, format string, users []User) {
+	var output io.Writer
+	if path != "" {
+		// write user info to path in specified format
+		f, err := os.Create(path)
+		HandleError(err)
+		defer f.Close()
+		output = f
+	} else {
+		// write user info to stdout
+		output = os.Stdout
+	}
+
+	if format == "csv" {
+		output.Write([]byte("id,name,home,shell\n"))
+		writer := csv.NewWriter(output)
+		for _, user := range users {
+			err := writer.Write([]string{strconv.Itoa(user.Id), user.Name, user.Home, user.Shell})
+			HandleError(err)
+		}
+		writer.Flush()
+	}
+
+	if format == "json" {
+		data, err := json.MarshalIndent(users, "", "  ")
+		HandleError(err)
+		output.Write(data)
+	}
+}
+
+func HandleError(err error) {
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
